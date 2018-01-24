52c527335e3ae922750b933663025988b2f6ee22
sashawants <35749366+sashawants@users.noreply.github.com>
2018-01-24 23:49:54 +0300

    Update viet.py

--- viet.py
+++ viet.py
@@ -1,30 +1,33 @@
 import math
 import cmath
 
-B,C=-(10)**(20),4.0
+B,C=3.0,4.0
 
 def rsh(b,c):
+    if (b==0) and (c==0):
+        root=0,0
+    else:
 
-    D=b**2-4*c
-    #print ('D = ',D)
+        D=b**2-4*c
+        #print ('D = ',D)
 
-    if D>=0:
-        if b>=0:
-            x1=(-b-math.sqrt(D))/2.0
-        else:
-            x1=(-b+math.sqrt(D))/2.0
-        x2=c/x1
+        if D>=0:
+            if b>=0:
+                x1=(-b-math.sqrt(D))/2.0
+            else:
+                x1=(-b+math.sqrt(D))/2.0
+            x2=c/x1
 
-        if x1==x2:
-            root=x1
-        else:
-            root=x1,x2
+            if x1==x2:
+                root=x1
+            else:
+                root=x1,x2
 
-    if D<0:
-        x1,x2=(-b+cmath.sqrt(D))/2.0 , (-b-cmath.sqrt(D))/2.0
-        #x1 = complex (round (x1.real , 2) , round (x1.imag , 2))
-        #x2 = complex (round (x2.real , 2) , round (x2.imag , 2))
-        root = x1,x2
+        if D<0:
+            x1,x2=(-b+cmath.sqrt(D))/2.0 , (-b-cmath.sqrt(D))/2.0
+            #x1 = complex (round (x1.real , 2) , round (x1.imag , 2))
+            #x2 = complex (round (x2.real , 2) , round (x2.imag , 2))
+            root = x1,x2
 
     return (root)
 
