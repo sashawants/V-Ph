5aec712d5b76d6408a010c5b243abda4b0133fda
sashawants <35749366+sashawants@users.noreply.github.com>
2018-01-24 23:55:22 +0300

    Update taylor.py

--- taylor.py
+++ taylor.py
@@ -1,8 +1,10 @@
 import math
 import cmath
 
-B,C=(10)**(20),3.0
-M=-4*C/(B**2)
+B,C=0.0,0.0
+
+if (b!=0)and(c!=0):
+    M=-4*C/(B**2)
 
 def bol(a):
     p=1
@@ -29,34 +31,37 @@ def row(x,n,m):
     return s
 
 def rsh(b,c):
+    
+    if (b==0)and(c==0):
+        root=0,0
+    else:
 
-    D=b**2-4*c
-    #print ('D = ',D)
+        D=b**2-4*c
+        #print ('D = ',D)
 
-    if D>=0:
-        if math.fabs(R)<=8:
-            x1,x2 = (-b+math.sqrt(D))/2.0 , (-b-math.sqrt(D))/2.0
-        else: 
-            print ('CHECK B')
-            S=row(M,R/2,0.5)       
-            x1=(B*S)/2.0
-            x2=c/x1
+        if D>=0:
+            if math.fabs(R)<=8:
+                x1,x2 = (-b+math.sqrt(D))/2.0 , (-b-math.sqrt(D))/2.0
+            else: 
+                print ('CHECK B')
+                S=row(M,R/2,0.5)       
+                x1=(B*S)/2.0
+                x2=c/x1
 
-        if x1==x2:
-            root=x1
-        else:
-            root=x1,x2
-
-    if D<0:
-        x1,x2=(-b+cmath.sqrt(D))/2.0 , (-b-cmath.sqrt(D))/2.0
-        #x1 = complex (round (x1.real , 2) , round (x1.imag , 2))
-        #x2 = complex (round (x2.real , 2) , round (x2.imag , 2))
-        root = x1,x2
-
+            if x1==x2:
+                root=x1
+            else:
+                root=x1,x2
+                
+        if D<0:
+            x1,x2=(-b+cmath.sqrt(D))/2.0 , (-b-cmath.sqrt(D))/2.0
+            #x1 = complex (round (x1.real , 2) , round (x1.imag , 2))
+            #x2 = complex (round (x2.real , 2) , round (x2.imag , 2))
+            root = x1,x2
     return (root)
 
 print (rsh(B,C))
+
 #print bol(B,C)
 #print 'row=',row(M,R,0.5)
 #print 'xd=',(C/B+(C**2)/(B**3))/B
-
